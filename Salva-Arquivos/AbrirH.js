9e60bcd8fa70954cf7ccb88ff239424634dc65a7
Pedro vinicius V7 <pedrn770@gmail.com>
2024-07-15 19:26:38 -0300

    Segunda-feira, 15 de julho de 2024

--- Salva-Arquivos/AbrirH.js
+++ Salva-Arquivos/AbrirH.js
@@ -2,4 +2,8 @@ function abriHTML(install){
     if(install === 'Abrir'){
         window.open('http://localhost:3000','_blank');
     }
+
+    if(install === 'files'){
+        window.open('http://localhost:3000/list.html','_blank');
+    }
 }
--- Salva-Arquivos/server.js
+++ Salva-Arquivos/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const multer = require('multer');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 const port = 3000;
@@ -23,6 +24,26 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Rota para upload de arquivos
 app.post('/upload', upload.single('file'), (req, res) => {
     res.json({ message: 'Upload bem-sucedido!', file: req.file });
+});
+
+// Rota para listar arquivos
+app.get('/files', (req, res) => {
+    fs.readdir('uploads/', (err, files) => {
+        if (err) {
+            return res.status(500).json({ error: 'Erro ao listar arquivos' });
+        }
+        res.json(files);
+    });
+});
+
+// Rota para baixar arquivos
+app.get('/download/:filename', (req, res) => {
+    const filePath = path.join(__dirname, 'uploads', req.params.filename);
+    res.download(filePath, (err) => {
+        if (err) {
+            return res.status(500).json({ error: 'Erro ao baixar o arquivo' });
+        }
+    });
 });
 
 app.listen(port, () => {
